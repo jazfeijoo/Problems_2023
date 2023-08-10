4cfd6c99384ab5671930865c22a48b31f621a62b
Jazmin Feijoo <jazminfeijoo@Jazmins-MBP.lan>
2023-08-10 19:39:14 -0400

    TwoSums: solved

---
+++ TwoSums.js
@@ -0,0 +1,19 @@
+var twoSum = function(nums, target) {
+
+
+  for (let i = 0; i < nums.length; i++){
+    for (let j = i +1; j <= nums.length; j++){
+      if (nums[i] + nums[j] === target){
+        return [i,j]
+      }
+    }
+  }
+
+};
+
+let test1 = twoSum([-1,-2,-3,-4,-5], -8)
+console.log('TEST 1:', test1)
+// let test2 = twoSum([2,7,11,15], 9)
+// let test3 = twoSum([3,3], 6)
+
+
