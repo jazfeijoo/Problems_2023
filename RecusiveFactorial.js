598bd8cd7e37c6426761245d7763e706ff75ded7
Jazmin Feijoo <jazminfeijoo@Jazmins-MBP.lan>
2023-08-15 18:36:01 -0400

    RecursiveFactorial: solved

---
+++ RecusiveFactorial.js
@@ -0,0 +1,13 @@
+//Write recursive function that calculates the factorial of input n
+// 4! = 4 * 3 * 2 * 1
+
+function factorial (n) {
+ if (n===1){
+  return 1;
+ }
+
+ return n * factorial(n-1)
+}
+
+console.log(factorial(4)); //24
+console.log(factorial(6)); //720
